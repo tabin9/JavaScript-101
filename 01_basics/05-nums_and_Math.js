b4cce8fd3a1966843b9e9f0a614ee27ce0093ba8
Tabin Tariq <77425959+tabin9@users.noreply.github.com>
2023-11-23 11:26:04 +0000

    Maths and Numbers

--- 01_basics/05-nums_and_Math.js
+++ 01_basics/05-nums_and_Math.js
@@ -1,17 +1,17 @@
-const score = 400
+const score = 400;
 // console.log(score);
 
-const balance = new Number(100)
+const balance = new Number(100);
 // console.log(balance);
 
 // console.log(balance.toString().length);
 // console.log(balance.toFixed(1));
 
-const otherNumber = 123.8966
+const otherNumber = 123.8966;
 
 // console.log(otherNumber.toPrecision(4));
 
-const hundreds = 1000000
+const hundreds = 1000000;
 // console.log(hundreds.toLocaleString('en-IN'));
 
 // +++++++++++++ Maths +++++++++++++++++++++++++++++
@@ -34,4 +34,4 @@ console.log(Math.floor(Math.random()*10) + 1);
 const min = 10
 const max = 20
 
-console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
