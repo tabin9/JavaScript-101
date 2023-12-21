4974c3ff87de85622c778e71135a6d94581dc899
Tabin Tariq <77425959+tabin9@users.noreply.github.com>
2023-12-21 13:03:20 +0000

    Switch Statement an Break

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,23 @@
+
+// switch staement
+const month = "March"
+switch (month) {
+    case "Jan":
+        console.log("January");
+        break;
+    case "Feb":
+        console.log("Feb");
+        break;
+    case "March":
+        console.log("March");
+        break;
+    case "April":
+        console.log("April");
+        break;
+    default:
+        console.log("Default Month");
+        break;
+}
+
+// usage of break is well known --> breaks the flow
+// break and continue more in detail in loops section
